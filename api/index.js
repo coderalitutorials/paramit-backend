5bd37fd75b64e79a93da44a11a1a0667e74dc9f9
coderalitutorials <razamuneer670@gmail.com>
2026-06-02 05:09:41 -0700

    cors updation

--- api/index.js
+++ api/index.js
@@ -11,7 +11,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://paramit-frontend.vercel.app",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
